ba10f2e0ece430c914f4926380b990e23ed81404
Henrique Aron <rike.aron@gmail.com>
2021-01-15 13:21:19 -0300

    First commit, implementing event bus

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function ({ postId }) {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetchComments();
+  }, []);
+
+  const fetchComments = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    setComments(res.data);
+  };
+
+  const renderComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderComments}</ul>;
+}
